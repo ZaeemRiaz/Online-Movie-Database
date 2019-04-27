d5dab619139d586ac2179126fa44032eb7b3766a
Zaeem Riaz <riazzaeem0@hotmail.co.uk>
2019-04-27 14:29:07 +0500

    v1.2.1

--- DB_Project/Controllers/HomeController.cs
+++ DB_Project/Controllers/HomeController.cs
@@ -22,28 +22,30 @@ namespace DB_Project.Controllers
         {
             return View();
         }
+        public ActionResult Msg(int param)
+        {
+            return View(param);
+        }
         public ActionResult Signupfunc(string email, string name, string usertype, string dateOfBirth, string password)
         {
-            userProc uproc = new userProc();
-            ObjectParameter flag = new ObjectParameter("flag", typeof(int));
-            var value = uproc.add_user(name, usertype, email, password, dateOfBirth, flag);
-            int ret = Convert.ToInt32(flag.Value);
+            int ret;
+            //userProc uproc = new userProc();
+            //ObjectParameter flag = new ObjectParameter("flag", typeof(int));
+            //var value = uproc.add_user(name, usertype, email, password, dateOfBirth, flag);
+            //int ret = Convert.ToInt32(flag.Value);
 
             return RedirectToAction("Msg", new { param = ret });
         }
         public ActionResult Loginfunc(string email, string password)
         {
-            userProc uproc = new userProc();
-            ObjectParameter flag = new ObjectParameter("flag", typeof(int));
-            var value = uproc.login_user(email, password, flag);
-            int ret = Convert.ToInt32(flag.Value);
-            ret += 2;
+            int ret;
+            //userProc uproc = new userProc();
+            //ObjectParameter flag = new ObjectParameter("flag", typeof(int));
+            //var value = uproc.login_user(email, password, flag);
+            //int ret = Convert.ToInt32(flag.Value);
+            //ret += 2;
 
             return RedirectToAction("Msg", new { param = ret });
-        }
-        public ActionResult Msg(int param)
-        {
-            return View(param);
         }
     }
 }
