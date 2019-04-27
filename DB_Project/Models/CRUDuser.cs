f48be24ba9ce47c214f36f6cf645f22e0534fdf5
Zaeem Riaz <riazzaeem0@hotmail.co.uk>
2019-04-27 15:55:13 +0500

    v1.2.5

--- DB_Project/Models/CRUDuser.cs
+++ DB_Project/Models/CRUDuser.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DB_Project.Models
 {
     public class CRUDuser
     {
+        string connectionString = @"Data Source=localhost;Initial Catalog=muz;Intgrated Security=True;";
+        string queryString = "SELECT * FROM Users";
+
     }
 }
