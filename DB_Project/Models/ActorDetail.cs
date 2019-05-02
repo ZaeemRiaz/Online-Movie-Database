6ad82882991c915ef71a29c4e8e8af69ea657858
Zaeem Riaz <riazzaeem0@hotmail.co.uk>
2019-05-02 09:25:28 +0500

    v1.4.14

---
+++ DB_Project/Models/ActorDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DB_Project.Models
+{
+    public class ActorDetail
+    {
+        public String name { get; set; }
+        public String bdate { get; set; }
+        public String gender { get; set; }
+        public String description { get; set; }
+    }
+}
--- DB_Project/Models/CRUDactor.cs
+++ DB_Project/Models/CRUDactor.cs
@@ -16,8 +16,38 @@ namespace DB_Project.Models
         }
         public static int DelActorFunc(string email, string password)
         {
+            //open connection to db
+            string connectionString = @"Data Source=localhost;Initial Catalog=muz;Integrated Security=True;";
 
-            return 0;
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command;
+            int result = 0;
+
+            //try execution
+            try
+            {
+                connection.Open();
+
+                command = new SqlCommand("delete_user", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add("@userID", SqlDbType.Int).Value = userID;
+
+                command.Parameters.Add("@flag", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                command.ExecuteNonQuery();
+                result = Convert.ToInt32(command.Parameters["@flag"].Value);
+            }
+            catch (SqlException ex)//print error message
+            {
+                Console.WriteLine("SQL Error" + ex.Message.ToString());
+                result = -1; //-1 will be interpreted as "error while connecting with the database."
+            }
+            finally//close connection
+            {
+                connection.Close();
+            }
+            return result;
         }
         public static int AddActorFunc(string email, string password)
         {
