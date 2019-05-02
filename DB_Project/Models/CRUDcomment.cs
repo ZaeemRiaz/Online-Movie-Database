cdd1a9d9a8a22459d5290498c6cd08adcd7b5a2f
Zaeem Riaz <riazzaeem0@hotmail.co.uk>
2019-05-02 21:30:24 +0500

    v1.6.3

--- DB_Project/Models/CRUDcomment.cs
+++ DB_Project/Models/CRUDcomment.cs
@@ -9,14 +9,51 @@ namespace DB_Project.Models
 {
     public class CRUDcomment
     {
-        public static int AddCommentFunc(string email, string password)
+        public static int AddCommentFunc(string movieID, string userID, string comment)
         {
+            //open connection to db
+            string connectionString = @"Data Source=localhost;Initial Catalog=muz;Integrated Security=True;";
 
-            return 0;
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = null;
+            int result = 0;
+
+            //try execution
+            try
+            {
+                connection.Open();
+
+                command = new SqlCommand("add_comment", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add("@movieID", SqlDbType.Int).Value = movieID;
+                command.Parameters.Add("@userID", SqlDbType.Int).Value = userID;
+                command.Parameters.Add("@comment", SqlDbType.VarChar, 300).Value = comment;
+
+                command.Parameters.Add("@flag", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                command.ExecuteNonQuery();
+                result = Convert.ToInt32(command.Parameters["@flag"].Value);
+            }
+            catch (SqlException ex)//print error message
+            {
+                Console.WriteLine("SQL Error" + ex.Message.ToString());
+                result = -1; //-1 will be interpreted as "error while connecting with the database."
+            }
+            finally//close connection
+            {
+                // close connection
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+            return result;
         }
         public static int MovieCommentFunc(string email, string password)
         {
-
+            //comments_movie
+            //@mID int
             return 0;
         }
     }
--- DB_Project/Models/CRUDrating.cs
+++ DB_Project/Models/CRUDrating.cs
@@ -9,15 +9,46 @@ namespace DB_Project.Models
 {
     public class CRUDrating
     {
-        public static int AddRatingFunc(string email, string password)
+        public static int AddRatingFunc(string movieID, string userID, string rating)
         {
+            //open connection to db
+            string connectionString = @"Data Source=localhost;Initial Catalog=muz;Integrated Security=True;";
 
-            return 0;
-        }
-        public static int CalRatingFunc(string email, string password)
-        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = null;
+            int result = 0;
 
-            return 0;
+            //try execution
+            try
+            {
+                connection.Open();
+
+                command = new SqlCommand("add_rating", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add("@movieID", SqlDbType.Int).Value = movieID;
+                command.Parameters.Add("@userID", SqlDbType.Int).Value = userID;
+                command.Parameters.Add("@rating", SqlDbType.Int).Value = rating;
+
+                command.Parameters.Add("@flag", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                command.ExecuteNonQuery();
+                result = Convert.ToInt32(command.Parameters["@flag"].Value);
+            }
+            catch (SqlException ex)//print error message
+            {
+                Console.WriteLine("SQL Error" + ex.Message.ToString());
+                result = -1; //-1 will be interpreted as "error while connecting with the database."
+            }
+            finally//close connection
+            {
+                // close connection
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+            return result;
         }
     }
 }
