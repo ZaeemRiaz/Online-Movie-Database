3aa31656d02f363ea9d82c805ff8bd66c99a8363
Zaeem Riaz <riazzaeem0@hotmail.co.uk>
2019-05-06 21:30:05 +0500

    v1.10.4

--- DB_Project/Models/CRUDcomment.cs
+++ DB_Project/Models/CRUDcomment.cs
@@ -52,7 +52,6 @@ namespace DB_Project.Models
         }
         public static List<Comment> MovieCommentFunc(string movieID)
         {
-            int mID = Int32.Parse(movieID);
             List<Comment> cList = new List<Comment>();
             //open connection to db
             string connectionString = @"Data Source=localhost;Initial Catalog=muz;Integrated Security=True;";
@@ -68,7 +67,7 @@ namespace DB_Project.Models
 
                 command = new SqlCommand("comments_movie", connection);
                 command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.Add("@mID", SqlDbType.Int).Value = mID;
+                command.Parameters.Add("@mID", SqlDbType.Int).Value = movieID;
                 reader = command.ExecuteReader();
                 Comment c;
                 while (reader.Read())
